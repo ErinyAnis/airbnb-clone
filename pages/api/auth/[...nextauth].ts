8e3a590c00e3c7c55865850fd7b4c7b11ff47e99
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2024-11-05 16:30:15 +0200

    touches

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -16,6 +16,11 @@ export const authOptions: AuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          scope: "openid profile email",
+        },
+      },
     }),
     CredentialsProvider({
       name: "credentials",
@@ -56,4 +61,7 @@ export const authOptions: AuthOptions = {
   },
   secret: process.env.NEXTAUTH_SECRET,
 };
+
+console.log("Google Client ID:", process.env.GOOGLE_CLIENT_ID);
+console.log("Google Client Secret:", process.env.GOOGLE_CLIENT_SECRET);
 export default NextAuth(authOptions);
